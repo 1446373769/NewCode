b310b37f04771d678e964cb92c5d472da0457fc9
zjq tj <1446373769@qq.com>
2019-04-15 20:38:37 +0800

    响应式内容提交

---
+++ Resp-dev/src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    $("#ktBanner").owlCarousel({
+        items: 1,
+        loop: true,
+        center: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
